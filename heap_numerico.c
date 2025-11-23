08e9ee750ae0df6828ff9d658cf482865c15b40c
avwong <alinarodriguezwong@gmail.com>
2025-11-22 22:37:44 -0600

    Pasar nodos directo a intercambiar

--- heap_numerico.c
+++ heap_numerico.c
@@ -9,15 +9,80 @@ struct heap_numerico* crear_heap_numerico(int capacidad_inicial) {
 };
 
 /*
-E: puntero de nodos a intercambiar
+E: puntero al heap , indices de los nodos a intercambiar
 S: void
-R: las entradas deben ser nodos 
+R: el heap debe existir, los indices deben ser validos
 */
-void intercambiar_nodos_numerico(struct nodo_heap_numerico* a, struct nodo_heap_numerico* b) {
-    struct nodo_heap_numerico temp = *a;
-    *a = *b;
-    *b = temp;
+void intercambiar_nodos_numerico(struct heap_numerico* heap, int i, int j) {
+    
+    //validaciones
+    if (heap == NULL || i < 0 || j < 0 || i >= heap->tamano || j >= heap->tamano) {
+        printf("Error: no se pudo intercambiar nodos numericos, indices invalidos.\n");
+        return;
+    }
+    
+    struct nodo_heap_numerico temp = heap->nodos[i];
+    heap->nodos[i] = heap->nodos[j];
+    heap->nodos[j] = temp;
 }
 
-void 
+/*
+Asegura que siempre haya espacio en el heap para agregar un nodo mas en caso de que se llegue a llenar el heap
+E: puntero al heap numérico 
+S: void
+R: que exista el heap, que tenga capacidad mayor a 0
+*/
+void asegurar_capacidad_numerico(struct heap_numerico* heap) {
+    
+    //validacion: que exista el heap
+    if (heap == NULL){
+        return;
+    }
+
+    //si aun hay campo, no se ocupa
+    if (heap->tamano < heap->capacidad) {
+        return;
+    }
+
+    //duplica la capacidad del heap
+    int nueva_capacidad = heap->capacidad * 2; 
+    
+    //validacion por si la nueva capacidad llega a ser inutil (no deberia suceder)
+    if (nueva_capacidad <= 0){
+        nueva_capacidad = 1;
+    }
+
+    //expande en donde ya esta
+    struct nodo_heap_numerico* nuevo = realloc(heap->nodos, nueva_capacidad * sizeof(struct nodo_heap_numerico));
+
+    if (nuevo == NULL) {
+        printf("Error: no se pudo redimensionar heap numerico.\n");
+        return;
+    }
+
+    //actualiza el heap
+    heap->nodos = nuevo;
+    heap->capacidad = nueva_capacidad;
+}
+
+/* 
+Intercambia un nodo hijo con su padre si el nodo hijo es menor que el padre
+E: heap y el indice del nodo a mover hacia arriba
+S: void
+R: que el heap exista, asegurarse que el indice sea valido
+*/
+void subir_numerico(struct heap_numerico* heap, int hijo) {
+    while (hijo > 0) {
+        int padre = (hijo - 1) / 2; //formula para encontrar al padre
+
+        //asegurarse que el indice del nodo del papa sea menor que el del hijo
+        //si el numero de palabras o año es menor, se intercambian
+        if (heap->nodos[hijo].llave < heap->nodos[padre].llave) {
+            intercambiar_nodos_numerico(heap, hijo, padre);
+            hijo = padre;
+        } else {
+            break;
+        }
+    }
+};
 
